443aad84ed99aeaf78646feed88d050e0a57b271
Mohammad Nikfallah <Nikfallahmohammad@gmail.com>
2022-03-29 03:25:14 +0430

    Banker.java init file added

---
+++ src/Banker.java
@@ -0,0 +1,10 @@
+import java.util.TreeMap;
+
+public class Banker {
+    private TreeMap<Integer,Player> deposits = new TreeMap<Integer,Player>();
+
+    public boolean swapWealth (Player player1,Player player2){
+        return false;
+    }
+
+}
