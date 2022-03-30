d249394ef628b0c978ae63b7fad743d455c049a6
Mohammad Nikfallah <Nikfallahmohammad@gmail.com>
2022-03-30 14:04:59 +0430

    comments added

--- src/Banker.java
+++ src/Banker.java
@@ -1,8 +1,10 @@
 import java.util.TreeMap;
 
 public class Banker {
+    //a list for deposits
     private TreeMap<Player,Integer> deposits = new TreeMap<Player,Integer>();
 
+    //swapping wealth for some reason :/
     public boolean swapWealth (Player player1,Player player2){
         if (player1.isGotBroke() || player2.isGotBroke()){
             return false;
@@ -15,6 +17,7 @@ public class Banker {
         return true;
     }
 
+    //checking for deposit and pay it
     public boolean checkDeposit(Player player){
         if (deposits.containsKey(player)){
             player.getPaid(deposits.get(player));
@@ -25,6 +28,7 @@ public class Banker {
         return false;
     }
 
+    //adding deposit for someone
     public void addDeposit(Player player){
         player.setMoneyDeposited(true);
         deposits.put(player,player.getBalance()/2);
