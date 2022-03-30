8b40c7acf371c2df55c6f6c2e658387ad736fc6d
Mohammad Nikfallah <Nikfallahmohammad@gmail.com>
2022-03-30 14:02:46 +0430

    deposits updated && checkDeposit & addDeposit added

--- src/Banker.java
+++ src/Banker.java
@@ -1,7 +1,7 @@
 import java.util.TreeMap;
 
 public class Banker {
-    private TreeMap<Integer,Player> deposits = new TreeMap<Integer,Player>();
+    private TreeMap<Player,Integer> deposits = new TreeMap<Player,Integer>();
 
     public boolean swapWealth (Player player1,Player player2){
         if (player1.isGotBroke() || player2.isGotBroke()){
@@ -15,4 +15,19 @@ public class Banker {
         return true;
     }
 
+    public boolean checkDeposit(Player player){
+        if (deposits.containsKey(player)){
+            player.getPaid(deposits.get(player));
+            player.setMoneyDeposited(false);
+            deposits.remove(player);
+            return true;
+        }
+        return false;
+    }
+
+    public void addDeposit(Player player){
+        player.setMoneyDeposited(true);
+        deposits.put(player,player.getBalance()/2);
+        player.pay(player.getBalance()/2);
+    }
 }
