900121381f65ccaba68373da27dbb774ae1e2c95
Mohammad Nikfallah <Nikfallahmohammad@gmail.com>
2022-03-30 13:41:00 +0430

    swapWealth added

--- src/Banker.java
+++ src/Banker.java
@@ -4,7 +4,15 @@ public class Banker {
     private TreeMap<Integer,Player> deposits = new TreeMap<Integer,Player>();
 
     public boolean swapWealth (Player player1,Player player2){
-        return false;
+        if (player1.isGotBroke() || player2.isGotBroke()){
+            return false;
+        }
+        int temp = player1.getBalance();
+        player1.pay(player1.getBalance());
+        player1.getPaid(player2.getBalance());
+        player2.pay(player2.getBalance());
+        player2.getPaid(temp);
+        return true;
     }
 
 }
