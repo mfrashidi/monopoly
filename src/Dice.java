bb83d7380cd29e3cebb05c5883a9b3489e9584f6
Foad Rashidi <mohammadfoadrashidi@gmail.com>
2022-04-01 10:41:21 +0430

    Change dice methods to static

--- src/Dice.java
+++ src/Dice.java
@@ -1,13 +1,7 @@
 import java.lang.Math;
 
 public class Dice {
-    private int diceRoll;
-
-    public int getDiceRoll() {
-        return diceRoll;
-    }
-
-    public void roll(){
-        int a = (int) (Math.random()*(6)+1);
+    public static int roll(){
+        return (int)(Math.random()*(6)+1);
     }
 }
