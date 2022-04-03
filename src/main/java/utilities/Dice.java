40336d950e63cf919056b1c1179c9cf69f62c95f
Foad Rashidi <mohammadfoadrashidi@gmail.com>
2022-04-03 19:38:33 +0430

    Enhance testing for the dice

--- src/main/java/utilities/Dice.java
+++ src/main/java/utilities/Dice.java
@@ -5,7 +5,13 @@ import java.lang.Math;
 
 public class Dice {
     public static int roll(boolean test) throws IOException {
-        if (test) return System.in.read() - 48;
+        if (test){
+            int input = -1;
+            while (input > 6 || input < 0){
+                input = System.in.read() - 48;
+            }
+            return input;
+        }
         else return (int)(Math.random()*(6)+1);
     }
 }
